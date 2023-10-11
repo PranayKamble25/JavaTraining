e03b024bdf646ba558f160b2ea30c499b19a7f37
PranayKamble25 <138237883+PranayKamble25@users.noreply.github.com>
2023-10-11 12:35:43 +0530

    Add files via upload
    Commit

---
+++ tns/DayOne/MyFirstProgram.java
@@ -0,0 +1,14 @@
+package com.DayOne;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			
+			System.out.println("Hello To My Java Learning!");
+		}
+
+	}
+
+
